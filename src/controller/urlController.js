ea0aac382e1156782ed7ed8653408c9d205dc5bb
karthi909 <karthikeyareddy440@gmail.com>
2022-05-17 15:57:17 +0530

    done

---
+++ src/controller/urlController.js
@@ -0,0 +1,78 @@
+const urlMOdel = require("../model/urlModel")
+// const nanoid = require('nanoid')
+//var { nanoid } = require("nanoid");
+const validUrl = require('valid-url')
+
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && value.trim().length === 0) return false;
+    return true;
+};
+
+const { customAlphabet } = require ('nanoid')
+
+
+
+const createUrl = async function (req, res) {
+    try{
+
+       
+       const data = req.body;
+
+        if (Object.keys(data).length == 0)  return res.status(400).send({ status: false, message: 'No data provided' }) 
+
+        const longUrl = req.body.longUrl
+
+
+     
+
+        if (!isValid(longUrl))  return res.status(400).send({ status: false, message: 'Long Url is required' }) 
+
+        if (!validUrl.isUri(longUrl))  return res.status(400).send({ status: false, message: 'Please provide a valid URL' }) 
+
+       
+
+
+        const baseUrl = 'http://localhost:3000';
+
+        if (!validUrl.isUri(baseUrl)) { return res.status(400).send({ status: false, message: 'The base URL is invalid' }) }
+
+
+      
+        //let codeurl = nanoid()
+        const nanoid = customAlphabet('abcdefghij', 10)
+        let codeurl = nanoid()
+        let urlCode = codeurl.toLowerCase()
+
+        const shorturl = baseUrl + '/' + urlCode
+
+       // console.log(shorturl)
+
+       data.shorturl = shorturl
+        console.log(data.shorturl)
+
+        data.urlCode = urlCode
+        console.log(data.urlCode)
+
+        let repeat = await urlMOdel.findOne({shorturl: data.shorturl, urlcode:data.urlCode})
+        console.log(repeat)
+        if(repeat) return res.status(400).send({status: false, msg:"not a unique shorturl and urlcode"})
+
+
+        await urlMOdel.create(data);
+        //console.log(newurl)
+
+        return res.status(201).send({status: true, msg:"URL created successfully", data: data})
+
+
+
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({status: false, msg: err.message})
+    }
+    
+}
+
+module.exports.createUrl = createUrl
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require("./Route/route.js")
+const { default: mongoose } = require('mongoose');
+const { Route } = require('express');
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+
+
+mongoose.connect("mongodb+srv://KA909_1:karthi123@cluster0.qpomb.mongodb.net/group99Database", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(3000)
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
